10b94b9be51879c2a4b68450ebcaeda7bc04650a
Fabian <Fabian@LAPTOP-10P412E5.hub>
2019-08-05 15:45:02 +1200

    Added turn rotations

--- src/Game.java
+++ src/Game.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Game {
-	
+
 	/* =========== fields =========== */
 	private List<Player> players;
 	private Player currentPlayer;
@@ -15,15 +15,16 @@ public class Game {
 	private List<Weapon> weapons;
 	private List<Character> characters;
 	private List<Room> rooms;
-	
+
 	public Game() {
 		board = new Board();
 		playerID = 0;
-		/* Initialize all weapon, character and room card lists
-		 * Doing both individual lists of weapon, character and room objects as well as
-		 * a single list of all cards for testing purposes to see which is better
+		/*
+		 * Initialize all weapon, character and room card lists Doing both individual
+		 * lists of weapon, character and room objects as well as a single list of all
+		 * cards for testing purposes to see which is better
 		 */
-		
+
 		players = new ArrayList<Player>();
 		weapons = new ArrayList<Weapon>(board.weapons);
 		characters = new ArrayList<Character>(board.characters);
@@ -33,55 +34,57 @@ public class Game {
 		Collections.shuffle(rooms);
 		Collections.shuffle(characters);
 		Collections.shuffle(cards);
-		
-		solution = new Suggestion(
-				weapons.remove(0),
-				characters.remove(0),
-				rooms.remove(0)
-				);
-		
+
+		solution = new Suggestion(weapons.remove(0), characters.remove(0), rooms.remove(0));
+
 		playerCount = 0;
-	    while (playerCount < 3) {
-	    	Scanner sc = new Scanner(System.in);
-	    	System.out.println("How many players? (3-6)");
-	    	playerCount = sc.nextInt();
-	    	System.out.println("Amount of players chosen: " + playerCount);
-	    }
-	    for(int i = 0; i<playerCount; i++) {
-	    	players.add(new Player(characters.get(i).getName(), i, this));
-	    	System.out.println("Player " + (i+1) + ": " + players.get(i).toString());
-	    }
-	    
-	    // Deal the cards, the cards list has already been shuffled
-	    playerID = 0;
-	    int cardsInHand = cards.size()/playerCount;
-	    for (int i = 0; i<playerCount; i++) {
-	    	for (int j = 0; j<cards.size()/playerCount; j++) {
-	    		players.get(i).addToHand(cards.get(j + i*cardsInHand));
-	    	}
-	    }
-	    // debugging purposes, check the hand of the first player
-	    System.out.println(players.get(0).printHand());
-	    
-	    for (int i=0; i<playerCount; ++i)
-	        players.get(i).spawn(board.playerSpawns.get(i));
-	    
-	    System.out.println(board.toString());
-	    
-	    currentPlayer = players.get(0);
-	    currentPlayer.newTurn();
+		while (playerCount < 3) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("How many players? (3-6)");
+			playerCount = sc.nextInt();
+			System.out.println("Amount of players chosen: " + playerCount);
+		}
+		for (int i = 0; i < playerCount; i++) {
+			players.add(new Player(characters.get(i).getName(), i, this));
+			System.out.println("Player " + (i + 1) + ": " + players.get(i).toString());
+		}
+
+		// Deal the cards, the cards list has already been shuffled
+		playerID = 0;
+		int cardsInHand = cards.size() / playerCount;
+		for (int i = 0; i < playerCount; i++) {
+			for (int j = 0; j < cards.size() / playerCount; j++) {
+				players.get(i).addToHand(cards.get(j + i * cardsInHand));
+			}
+		}
+		// debugging purposes, check the hand of the first player
+		System.out.println(players.get(0).printHand());
+
+		for (int i = 0; i < playerCount; ++i)
+			players.get(i).spawn(board.playerSpawns.get(i));
+
+		System.out.println(board.toString());
+		int p = 0;
+		while (true) {
+			while (p < playerCount) {
+				currentPlayer = players.get(p);
+				currentPlayer.newTurn();
+				p++;
+			}
+			p = 0;
+		}
 	}
+
 	public Board getBoard() {
 		return board;
-	
+
 	}
-	
+
 	public void setBoard(Board b) {
-		board = b; 
-		
-		
+		board = b;
+
 	}
-	
+
 	public static void main(String[] args) {
 		new Game();
 	}
